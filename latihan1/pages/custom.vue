a95e2ca6a059edf82ce704a502d4dd295ba37ff7
fachry99 <151fachry@gmail.com>
2023-11-30 22:25:49 +0700

    add tag img on pages>index.vue

--- latihan1/pages/custom.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-definePageMeta({
-  layout: "custom",
-});
-</script>
-
-<template>
-  <div class="Custom page">custom page from folder pages</div>
-</template>
--- latihan1/pages/index.vue
+++ latihan1/pages/index.vue
@@ -1,4 +1,7 @@
 //import Alert from '~/components/Alert.vue'
 <template>
-  <div class="Main">main page from folder pages</div>
+  <div class="Main">
+    <h3>hello nuxt3</h3>
+    <img src="/img/nuxt.png" alt="Discover Nuxt 3" />
+  </div>
 </template>
