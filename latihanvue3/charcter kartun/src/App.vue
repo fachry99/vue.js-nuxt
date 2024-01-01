7d8c9ef6706be96f9bf9d0362a7ce6fcb1061304
fachry99 <151fachry@gmail.com>
2024-01-01 19:13:55 +0700

    move character kartun project from CLI index.html to app.vue

---
+++ latihanvue3/charcter kartun/src/App.vue
@@ -0,0 +1,97 @@
+<script>
+export default {
+  data: () => ({
+    newCharacter: {
+      name: "",
+      element: [],
+    },
+    characterList: [
+      { name: "naruto", element: ["earth"] },
+      { name: "sasuke", element: ["fire"] },
+      { name: "sakura", element: ["water"] },
+      { name: "kakashi", element: ["lightning"] },
+      { name: "gaara", element: ["earth"] },
+    ],
+    favoriteList: [],
+  }),
+  computed: {
+    benderStatistic() {
+      const statistic = {};
+      this.characterList.forEach((character) => {
+        character.element.forEach((element) => {
+          if (statistic[element]) {
+            statistic[element]++;
+          } else {
+            statistic[element] = 1;
+          }
+        });
+      });
+      return statistic;
+    },
+  },
+  methods: {
+    addNewCharacter() {
+      this.characterList.push(this.newCharacter);
+      this.newCharacter = {
+        name: "",
+        element: [],
+      };
+    },
+    favoriteCharacter(character) {
+      this.favoriteList.push(character);
+    },
+  },
+};
+</script>
+
+<template>
+   <h1>exercise 3</h1>
+      <ul>
+        <li>{{benderStatistic}}</li>
+        <li v-for="(stat, type) in benderStatistic">{{type}}: {{stat}}</li>
+      </ul>
+      <p v-if="characterList.length === 0">there no character</p>
+      <ul v-else-if="characterList.length % 2 === 0">
+        <li v-for="character in characterList">
+          {{character.name}}
+          <button @click="favoriteCharacter(character)">favorite✨</button>
+        </li>
+      </ul>
+
+      <!-- favorite character -->
+      <h3>favorite Character</h3>
+      <p v-if="favoriteList.length === 0">there no favorite character</p>
+      <ul v-if="favoriteList.length > 0">
+        <li v-for="character in favoriteList">{{character.name}}</li>
+      </ul>
+      <p v-else>no favorite character</p>
+
+      <!-- new character -->
+      <h3>New Character</h3>
+      <p>{{newCharacter}}</p>
+      <label for="characterName">name: </label>
+      <input
+        type="text"
+        v-model="newCharacter.name"
+        @keyup.enter="addNewCharacter"
+      />
+      <br />
+      <label for="characterElement">element: </label>
+      <select v-model="newCharacter.element" @keyup.enter="addNewCharacter">
+        <option value="earth">earth</option>
+        <option value="fire">fire</option>
+        <option value="water">water</option>
+        <option value="lightning">lightning</option>
+      </select>
+      <br />
+      <button @click="addNewCharacter">Submit</button>
+
+      <p>
+        <span v-for="(character, index) in characterList"
+          >{{character.name}}{{index === characterList.length - 1? "" : ", "}}
+        </span>
+      </p>
+      <p v-if="characterList.length % 2  !== 0">there are odd character</p>
+      <p v-else>there are even character</p>
+</template>
+
