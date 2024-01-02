0b5d2fda1edf1d4ca0acf409ede400e4a9de62da
fachry99 <151fachry@gmail.com>
2024-01-02 08:20:12 +0700

    add custom component at sandbox folder

---
+++ latihanvue3/sandbox/src/components/baseCounter.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  data: () => ({
+      count: 10,
+      CounterTittle: 'Counter standard',
+      incrementAmount: 5,
+    }),
+    computed: {
+        displayTittle() {
+        if (this.count > 20) {
+            return 'counter - greater than 20'
+        } else {
+            return 'counter - less than 20'
+        }
+    },
+    optimizedIncrement() {
+        return this.displayTittle.length * this.incrementAmount;
+    },
+    },
+    methods: {
+        incrementCount(newAmount, event) {
+        console.log(event)
+        console.log(newAmount)
+        this.count += this.incrementAmount
+    }
+    },
+    watch: {
+        count(newValue) {
+        if (newValue > 20) {
+            this.CounterTittle += 'greater than 20'
+        }
+    }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ displayTittle }}</h1>
+  <p :data-increment-by="incrementAmount">{{ count }}</p>
+  <button @click="incrementCount">Increment</button>
+  <h1>{{ incrementAmount }}</h1>
+  <div>
+    <label for="incrementAmount">increment by:</label>
+    <input type="number" v-model.number="incrementAmount" />
+  </div>
+
+</template>
