f350db776c9841bec371ffafe73ac4583f04c4a9
fachry99 <151fachry@gmail.com>
2024-01-10 12:10:55 +0700

    emmit event

--- latihanvue3/sandbox/src/App.vue
+++ latihanvue3/sandbox/src/App.vue
@@ -1,20 +1,42 @@
 <script>
-import baseCounter from './components/baseCounter.vue';
+import baseCounter from './components/baseCounter.vue'
+import useCard from './components/use-card.vue'
 export default {
   components: {
     baseCounter,
+    useCard
   },
   data() {
     return {
       message: 'Hello it works',
-      listofitems: ['item1', 'item2', 'item3']
+      listofitems: ['item1', 'item2', 'item3'],
+      userData: {
+        name: 'John Doe',
+        gender: 'male',
+        favoriteColor: 'blue',
+        favotiteNumber: [10, 9]
+      }
     }
   },
-};
+  computed: {
+    refineUserData() {
+      return {
+        name: this.userData.name,
+        gender: this.userData.gender
+      }
+    }
+  },
+  methods: {
+    changeName() {
+      this.userData.name = 'fachry'
+    }
+  }
+}
 </script>
 
 <template>
-  <baseCounter/>
+  <useCard :user="refineUserData" @change-name="changeName" />
+  <baseCounter />
   <hr />
   <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
   <p v-else>odd: {{ message.toLowerCase() }}</p>
---
+++ latihanvue3/sandbox/src/components/use-card.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  emits: ['change-name']
+}
+</script>
+<template>
+  <p>User: {{ user.name }}</p>
+  <p>gender: {{ user.gender }}</p>
+  <button @click="$emit('change-name')">Change Name</button>
+</template>
