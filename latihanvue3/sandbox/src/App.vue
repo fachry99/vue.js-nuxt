08d26fc3461b89dc31c67c4fe3f66710809021a3
fachry99 <151fachry@gmail.com>
2024-01-17 10:49:17 +0700

    learn slots for layout

--- latihanvue3/sandbox/src/App.vue
+++ latihanvue3/sandbox/src/App.vue
@@ -1,8 +1,12 @@
 <script>
+import baseLayout from './components/base-layout.vue'
+import baseButton from './components/baseButton.vue'
 import baseCounter from './components/baseCounter.vue'
 import useCard from './components/use-card.vue'
 export default {
   components: {
+    baseLayout,
+    baseButton,
     baseCounter,
     useCard
   },
@@ -35,8 +39,17 @@ export default {
 </script>
 
 <template>
-  <useCard :user="refineUserData" @change-name="changeName" />
-  <baseCounter />
+  <baseLayout>
+    <template #sidebar>Aside</template>
+    <template #main>
+      <useCard :user="refineUserData" @change-name="changeName" />
+    </template>
+    <template #footer>
+      <baseCounter />
+    </template>
+  </baseLayout>
+  <baseButton :left="false"></baseButton>
+  <baseButton :left="true"></baseButton>
   <hr />
   <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
   <p v-else>odd: {{ message.toLowerCase() }}</p>
---
+++ latihanvue3/sandbox/src/components/baseButton.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  props: {
+    left: Boolean,
+    default: true
+  }
+}
+</script>
+
+<template>
+  <button><slot v-if="left">Submit</slot> <slot v-else>cancel</slot></button>
+</template>
