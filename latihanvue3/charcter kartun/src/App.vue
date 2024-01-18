a72bbe180f895c9c33ec13ee8cf2a6ba0b7c675e
fachry99 <151fachry@gmail.com>
2024-01-18 14:14:39 +0700

    Layout slots exercise

--- latihanvue3/charcter kartun/src/App.vue
+++ latihanvue3/charcter kartun/src/App.vue
@@ -1,9 +1,11 @@
 <script>
+import baseLayout from './components/baseLayout.vue'
 import favoriteCharacter from './components/favoriteCharacter.vue'
 import characterCard from './components/characterCard.vue'
 import BenderStatistics from './components/BenderStatistics.vue'
 export default {
   components: {
+    baseLayout,
     favoriteCharacter,
     characterCard,
     BenderStatistics
@@ -38,6 +40,33 @@ export default {
 
 <template>
   <h1>exercise 3</h1>
+
+  <baseLayout>
+    <template #one>
+      <h3>New Character</h3>
+      <p>{{ newCharacter }}</p>
+      <label for="characterName">name: </label>
+      <input type="text" v-model="newCharacter.name" @keyup.enter="addNewCharacter" />
+      <br />
+      <label for="characterElement">element: </label>
+      <select v-model="newCharacter.element" @keyup.enter="addNewCharacter">
+        <option value="earth">earth</option>
+        <option value="fire">fire</option>
+        <option value="water">water</option>
+        <option value="lightning">lightning</option>
+      </select>
+      <br />
+      <button @click="addNewCharacter">Submit</button>
+
+      <p>
+        <span v-for="(character, index) in characterList" :key="`comma-list-character-${index}`"
+          >{{ character.name }}{{ index === characterList.length - 1 ? '' : ', ' }}
+        </span>
+      </p>
+      <p v-if="characterList.length % 2 !== 0">there are odd character</p>
+      <p v-else>there are even character</p></template
+    >
+  </baseLayout>
   <BenderStatistics :characterList="characterList" />
 
   <!-- character list -->
@@ -51,26 +80,4 @@ export default {
   <!-- favorite character -->
   <favorite-character />
   <!-- new character -->
-  <h3>New Character</h3>
-  <p>{{ newCharacter }}</p>
-  <label for="characterName">name: </label>
-  <input type="text" v-model="newCharacter.name" @keyup.enter="addNewCharacter" />
-  <br />
-  <label for="characterElement">element: </label>
-  <select v-model="newCharacter.element" @keyup.enter="addNewCharacter">
-    <option value="earth">earth</option>
-    <option value="fire">fire</option>
-    <option value="water">water</option>
-    <option value="lightning">lightning</option>
-  </select>
-  <br />
-  <button @click="addNewCharacter">Submit</button>
-
-  <p>
-    <span v-for="(character, index) in characterList" :key="`comma-list-character-${index}`"
-      >{{ character.name }}{{ index === characterList.length - 1 ? '' : ', ' }}
-    </span>
-  </p>
-  <p v-if="characterList.length % 2 !== 0">there are odd character</p>
-  <p v-else>there are even character</p>
 </template>
---
+++ latihanvue3/charcter kartun/src/components/baseLayout.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <div class="grid">
+    <div><slot name="one"></slot></div>
+    <div><slot name="two"></slot></div>
+  </div>
+</template>
+
+<style>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  border: 5px solid blue;
+}
+</style>
