bca33de5f227a4ef21496cf95e76549df10f9412
fachry99 <151fachry@gmail.com>
2024-01-20 10:09:30 +0700

    feature: composition API with suspense

--- latihanvue3/sandbox/src/App.vue
+++ latihanvue3/sandbox/src/App.vue
@@ -1,23 +1,20 @@
 <script>
+import pokedex from './components/pokedex.vue'
+
 export default {
-  data: () => ({
-    pokedex: [1, 2, 3]
-  }),
-  methods: {
-    async fetchPokemon() {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-      const data = await response.json()
-      this.pokedex = data.results
-    }
-  },
-  mounted() {
-    this.fetchPokemon()
+  components: {
+    pokedex
   }
 }
 </script>
 
 <template>
   <h1>New App</h1>
-  <pre>{{ pokedex }}</pre>
-  <button @click="fetchPokemon">fetchPokemon</button>
+  <Suspense>
+    <pokedex />
+
+    <template #fallback>
+      <p>Loading...</p>
+    </template>
+  </Suspense>
 </template>
---
+++ latihanvue3/sandbox/src/components/pokedex.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  async setup() {
+    const regionName = 'Kanto'
+
+    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) =>
+      response.json()
+    )
+    return {
+      pokedex,
+      regionName
+    }
+  },
+  created() {
+    console.log(this.regionName)
+    console.log(this.pokedex)
+  }
+}
+</script>
+
+<template>
+  <pre>{{ pokedex }}</pre>
+</template>
