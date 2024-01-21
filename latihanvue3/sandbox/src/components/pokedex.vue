5d246691e5d742764a67e4e4f935d172151881ea
fachry99 <151fachry@gmail.com>
2024-01-21 13:41:00 +0700

    add: reactive references

--- latihanvue3/sandbox/src/components/pokedex.vue
+++ latihanvue3/sandbox/src/components/pokedex.vue
@@ -1,14 +1,33 @@
 <script>
+import { computed, ref, reactive } from 'vue'
 export default {
   async setup() {
-    const regionName = 'Kanto'
+    const regionName = ref('Kanto')
+    const state = reactive({
+      elementType: 'siapa, ya?'
+    })
+
+    console.log(state.elementType)
+
+    const regionNameAllCaps = computed(() => regionName.value.toUpperCase())
 
     const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((response) =>
       response.json()
     )
     return {
       pokedex,
-      regionName
+      regionName,
+      regionNameAllCaps
+    }
+  },
+  computed: {
+    regionNameLowerCase() {
+      return this.regionName.toLowerCase()
+    }
+  },
+  methods: {
+    changeRegionName() {
+      this.regionName = 'Samsul'
     }
   },
   created() {
@@ -19,5 +38,9 @@ export default {
 </script>
 
 <template>
+  <h2>{{ regionName }}</h2>
+  <h2>{{ regionNameAllCaps }}</h2>
+  <h2>{{ regionNameLowerCase }}</h2>
+  <button @click="changeRegionName">Change region name</button>
   <pre>{{ pokedex }}</pre>
 </template>
