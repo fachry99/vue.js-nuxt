a30200d8348d83d5a0948f3af983bb48820ef026
fachry99 <151fachry@gmail.com>
2024-01-22 10:01:51 +0700

    update: define props for title users

--- latihanvue3/Api_project/src/components/userPage.vue
+++ latihanvue3/Api_project/src/components/userPage.vue
@@ -1,34 +1,31 @@
-<script>
+<script setup>
 import { reactive } from 'vue'
 import userCard from './userCard.vue'
-export default {
-  components: {
-    userCard
-  },
-  async setup() {
-    const state = reactive({
-      userList: []
-    })
 
-    async function getUser() {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
-        res.json()
-      )
-      return response
-    }
-
-    state.userList = await getUser()
-    return {
-      state,
-      getUser
-    }
+defineProps({
+  title: {
+    type: String,
+    default: 'Users'
   }
+})
+
+const state = reactive({
+  userList: []
+})
+
+async function getUser() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
+    res.json()
+  )
+  return response
 }
+
+state.userList = await getUser()
 </script>
 
 <template>
   <main>
-    <h1>Users</h1>
+    <h1>{{ title }}</h1>
     <ul>
       <user-card v-for="user in state.userList" :user="user" :key="`user-${user.id}`" />
     </ul>
