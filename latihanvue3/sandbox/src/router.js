f14eb3437d8c0064eebfb2914ce41934862dbf4f
fachry99 <151fachry@gmail.com>
2024-02-02 15:45:10 +0700

    feat: dynamic route params with ID at pokemonPage

--- latihanvue3/sandbox/src/router.js
+++ latihanvue3/sandbox/src/router.js
@@ -1,7 +1,8 @@
 import pokedexPageVue from './views/pokedexPage.vue'
 import homePage from './views/homePage.vue'
-
+import pokemonPage from './views/pokemonPage.vue'
 export const routes = [
   { path: '/', component: homePage },
-  { path: '/pokodex', component: pokedexPageVue }
+  { path: '/pokedex', component: pokedexPageVue },
+  { path: '/pokedex/:id', component: pokemonPage }
 ]
--- latihanvue3/sandbox/src/views/pokedexPage.vue
+++ latihanvue3/sandbox/src/views/pokedexPage.vue
@@ -1,1 +1,7 @@
-<template><h1>this pokodex page</h1></template>
+<script setup>
+//go to the pokedex/1
+</script>
+<template>
+  <h1>this pokodex page</h1>
+  <router-link to="/pokedex/10">Go to pokedex #10</router-link>
+</template>
